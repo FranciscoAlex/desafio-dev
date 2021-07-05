06b525402028c5fbb08decef898e04f2a5c13ebf
FranciscoAlex <57793501+FranciscoAlex@users.noreply.github.com>
2021-07-05 17:41:37 +0100

    Add files via upload
    parsing data strusture from file

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request, redirect, flash
 import requests
 import re
+import pandas as pd
+# import mysql.connector
+
+# config = {
+#   'user': 'root',
+#   'password': 'root',
+#   'host': 'localhost:8889',
+#   'database': 'inventory',
+#   'raise_on_warnings': True,
+# }
+
+# link = mysql.connector.connect(**config)
+
 
 app = Flask(__name__, template_folder='templates_folder')
 
@@ -8,12 +21,20 @@ url =  "https://raw.githubusercontent.com/ByCodersTec/desafio-ruby-on-rails/mast
 response = requests.get(url)
 responselist = re.split('\n', response.text)
 
+tipo = responselist[0][0]
+Data = responselist[0][1:9]
+Valor = responselist[0][9:19]
+CPF = responselist[0][19:30]
+Cart = responselist[0][30:42]
+Hora = responselist[0][42:48]
+DonoDaLoja = responselist[0][48:62]
+NomeLoja = responselist[2][62:81]
 
 
 
 @app.route("/")
 def index():
-  return render_template('index.html', data=responselist[0])
+  return render_template('index.html', data=NomeLoja)
 
 if __name__ == "__main__":
  app.run()
